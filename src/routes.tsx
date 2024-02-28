8df60c90f6314ac4c228cadcc965cff127904baf
Bruno <brunomonteiromestanza@gmail.com>
2024-02-28 11:39:21 -0300

    fix: remove auth layout and auth pages from routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { About } from './pages/app/about'
 import { Home } from './pages/app/home'
-import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
 import { NotFound } from './pages/errors/404'
 import { ErrorPage } from './pages/errors/error'
 import { AppLayout } from './pages/layouts/app'
-import { AuthLayout } from './pages/layouts/auth'
 
 export const router = createBrowserRouter([
   {
@@ -17,14 +14,6 @@ export const router = createBrowserRouter([
     children: [
       { path: '/', element: <Home /> },
       { path: '/sobre-o-flowmodoro', element: <About /> },
-    ],
-  },
-  {
-    path: '/',
-    element: <AuthLayout />,
-    children: [
-      { path: '/sign-in', element: <SignIn /> },
-      { path: '/sign-up', element: <SignUp /> },
     ],
   },
   {
